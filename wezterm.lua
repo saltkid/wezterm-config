63d3a088f41909659e9c968ec53ef4f90a8eaa17
saltkid <ojisaltkid@gmail.com>
2025-01-09 14:46:30 +0800

    update bagman setup; add window's home path

--- wezterm.lua
+++ wezterm.lua
@@ -21,22 +21,42 @@ config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.force_reverse_video_cursor = true
 config.cursor_blink_rate = 0
+
+-- WINDOWS SPECIFIC
+if wezterm.target_triple:find("windows") then
+    config.default_domain = "WSL:Debian"
+end
 -- }}}
 
 -- CYCLE BG IMAGES {{{
 wezterm.plugin.require("https://github.com/saltkid/bagman").setup({
     dirs = {
-        os.getenv("HOME") .. "/pictures/wt_bg/center-uniform-0.1",
-        os.getenv("HOME") .. "/pictures/wt_bg/center-uniformToFill-0.1",
         {
-            path = os.getenv("HOME") .. "/pictures/wt_bg/right-uniform-0.1",
+            path = wezterm.home_dir .. "/Pictures/wt_bg/center-uniform-0.1",
+            opacity = 0.1,
+        },
+        {
+            path = wezterm.home_dir
+                .. "/Pictures/wt_bg/center-uniformToFill-0.1",
+            object_fit = "Cover",
+            opacity = 0.1,
+        },
+        {
+            path = wezterm.home_dir .. "/Pictures/wt_bg/right-uniform-0.1",
+            vertical_align = "Bottom",
             horizontal_align = "Right",
+            opacity = 0.1,
         },
     },
+    backdrop = {
+        color = colors.background,
+        opacity = 0.95,
+    },
     interval = 30 * 60,
-    backdrop = colors.background,
-    loop_on_startup = true,
     change_tab_colors = true,
+    -- experimental = {
+    --     contain_fix_wezterm_build = true,
+    -- },
 })
 -- }}}
 
